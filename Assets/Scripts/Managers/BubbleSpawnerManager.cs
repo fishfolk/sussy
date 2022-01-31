c737440dee6b21207ad3d5477fe12526e0c54eef
FadiAshy <88489396+FadrikAlexanderPrivate@users.noreply.github.com>
2022-01-31 21:56:30 +0200

    Decorations Added

---
+++ Assets/Scripts/Managers/BubbleSpawnerManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleSpawnerManager : MonoBehaviour
+{
+    [SerializeField] List<GameObject> BubblesList;
+
+    private void Awake()
+    {
+        StartCoroutine(spawnBubbles());
+    }
+    IEnumerator spawnBubbles()
+    {
+        WaitForSeconds delay = new WaitForSeconds(5);
+
+        while (true)
+        {
+            respawnBubbles();
+            yield return delay;
+        }
+    }
+
+    void respawnBubbles()
+    {
+        foreach (var v in BubblesList)
+        {
+            v.transform.localPosition = Random.insideUnitCircle * 25;
+        }
+    }
+}
